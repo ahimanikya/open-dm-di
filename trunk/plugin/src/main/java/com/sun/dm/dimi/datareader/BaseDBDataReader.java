8446490c03931367bd878ede2962d94f47d5cec0
bharanim <bharanim@77a3c157-be3e-0410-b79f-aee7da2dc004>
2008-01-31 12:54:17 +0000

    Adding Finalizer count

--- trunk/plugin/src/main/java/com/sun/dm/dimi/datareader/BaseDBDataReader.java
+++ trunk/plugin/src/main/java/com/sun/dm/dimi/datareader/BaseDBDataReader.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package com.sun.dm.dimi.datareader;
 
 import com.sun.dm.dimi.util.DBDelimiters;
@@ -24,36 +23,35 @@ import net.java.hulp.i18n.Logger;
  * @author Manish
  */
 public abstract class BaseDBDataReader implements GlobalDataObjectReader {
-    
+
     //List of Data Object Created
     List<DataObject> doLinkedList = Collections.synchronizedList(new LinkedList());
     // List of data objects and object nodes available for finalization
     List<Object> usedObjectsList = Collections.synchronizedList(new LinkedList());
     // Object Finalizer Slave Thread
-    ObjectFinalizerSlave objfin = null;    
+    ObjectFinalizerSlave objfin = null;
     private boolean specialMode = false;
     //Logger
     private static Logger sLog = LogUtil.getLogger(BaseDBDataReader.class.getName());
-   
-    
+
     /**
      * BaseDBDataReader Constructor
      */
     public BaseDBDataReader() {
-            // Create Finalizer Instance
-            objfin = new ObjectFinalizerSlave(this);
-            objfin.start();
-    }    
-    
+        // Create Finalizer Instance
+        objfin = new ObjectFinalizerSlave(this);
+        objfin.start();
+    }
+
     /**
      * BaseDBDataReader Constructor
      * @param specialmode 
      */
-    public BaseDBDataReader(boolean specialmode){
+    public BaseDBDataReader(boolean specialmode) {
         this();
         specialMode = specialmode;
     }
-        
+
     /**
      * check whether the full record or partial record has been read
      * @param record
@@ -64,15 +62,15 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
         for (int i = record.length() - 1; i > -1; i--) {
             if (record.charAt(i) == DBDelimiters.TILDA_CHAR) {
                 tildaCount++;
-            } else
+            } else {
                 break;
+            }
         }
-        
+
         int j = tildaCount % 2;
         return j == 0;
     }
-    
-    
+
     /**
      * Returns the new data record from the line read from the Good/Reject file
      * @param recordStr
@@ -81,18 +79,18 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
      */
     protected DataObject newDataObject(String recordStr) throws InvalidRecordFormat {
         // TODO Auto-generated method stub
-        
+
         DataObject r = new DataObject();
-        
+
         if (requireSpecialProcessing(recordStr)) {
             doSpecialProcessing(r, recordStr);
         } else {
             updateDataObject(r, recordStr);
         }
-        
+
         return r;
     }
-    
+
     /**
      * Returns if the record needs special processing
      * @param recordStr
@@ -100,10 +98,8 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
      */
     protected boolean requireSpecialProcessing(String recordStr) {
         return specialMode;
-    }    
-    
+    }
 
-    
     /**
      * Does special processing for the records read from the file
      * @param r
@@ -112,17 +108,17 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
      */
     private void doSpecialProcessing(DataObject r, String record)
             throws InvalidRecordFormat {
-        
+
         int i = doSpecialProcessingForFields(r, record);
-        
+
         if (i >= record.length()) {
             return;
         }
-        
+
         doSpecialProcessingForChildTypes(r, record.substring(i));
-        
+
     }
-    
+
     /**
      * Do Special Processing for Data Object fields
      * @param r
@@ -133,78 +129,76 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < record.length(); i++) {
             switch (record.charAt(i)) {
-                
-            case (DBDelimiters.PIPE_CHAR):
-                r.addFieldValue(sb.toString());
-                sb = new StringBuilder();
-                
-                break;
-                
-            case (DBDelimiters.HASH_CHAR):
-                r.addFieldValue(sb.toString());
-                return i;
-                
-            case (DBDelimiters.TILDA_CHAR):
-                i++;
-                sb.append(record.charAt(i));
-                break;
-                
-            default:
-                sb.append(record.charAt(i));
+
+                case (DBDelimiters.PIPE_CHAR):
+                    r.addFieldValue(sb.toString());
+                    sb = new StringBuilder();
+
+                    break;
+
+                case (DBDelimiters.HASH_CHAR):
+                    r.addFieldValue(sb.toString());
+                    return i;
+
+                case (DBDelimiters.TILDA_CHAR):
+                    i++;
+                    sb.append(record.charAt(i));
+                    break;
+
+                default:
+                    sb.append(record.charAt(i));
             }
         }
         r.addFieldValue(sb.toString());
         return record.length();
     }
-    
+
     /**
      * Do Special Processing for Child Types
      */
     private void doSpecialProcessingForChildTypes(DataObject parent,
             String record) throws InvalidRecordFormat {
-        
+
         StringBuilder sb = new StringBuilder();
-        
+
         ChildType ct = null;
         DataObject child = null;
         for (int i = 0; i < record.length(); i++) {
             switch (record.charAt(i)) {
-                
-            case (DBDelimiters.PIPE_CHAR):
-                child.addFieldValue(sb.toString());
-                sb = new StringBuilder();
-                
-                break;
-                
-            case (DBDelimiters.DOLLAR_CHAR):
-                if (child != null) {
+
+                case (DBDelimiters.PIPE_CHAR):
                     child.addFieldValue(sb.toString());
                     sb = new StringBuilder();
-                }
-                child = new DataObject();
-                ct.addChild(child);
-                break;
-            case (DBDelimiters.HASH_CHAR):
-                ct = new ChildType();
-                parent.addChildType(ct);
-                
-                break;
-                
-            case (DBDelimiters.TILDA_CHAR):
-                i++;
-                sb.append(record.charAt(i));
-                break;
-                
-            default:
-                sb.append(record.charAt(i));
+
+                    break;
+
+                case (DBDelimiters.DOLLAR_CHAR):
+                    if (child != null) {
+                        child.addFieldValue(sb.toString());
+                        sb = new StringBuilder();
+                    }
+                    child = new DataObject();
+                    ct.addChild(child);
+                    break;
+                case (DBDelimiters.HASH_CHAR):
+                    ct = new ChildType();
+                    parent.addChildType(ct);
+
+                    break;
+
+                case (DBDelimiters.TILDA_CHAR):
+                    i++;
+                    sb.append(record.charAt(i));
+                    break;
+
+                default:
+                    sb.append(record.charAt(i));
             }
         }
-        
+
         child.addFieldValue(sb.toString());
     }
-    
 
-    
     /**
      * Update Data Objects
      * @param recordStr
@@ -212,7 +206,7 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
      */
     private void updateDataObject(DataObject r, String recordStr)
             throws InvalidRecordFormat {
-        
+
         try {
             int i = updateDataObjectFields(r, recordStr);
             if (i >= recordStr.length()) {
@@ -222,39 +216,39 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
         } catch (Exception e) {
             throw new InvalidRecordFormat(
                     "bad record format, use special mode to read records");
-            
+
         }
     }
-    
+
     /**
      * Update DataObject Fileds
      * @param r - Parent Data Object
      * @param record - String Record
      */
     private int updateDataObjectFields(DataObject r, String record) {
-        
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < record.length(); i++) {
             switch (record.charAt(i)) {
-                
-            case (DBDelimiters.PIPE_CHAR):
-                r.addFieldValue(sb.toString());
-                sb = new StringBuilder();
-                
-                break;
-                
-            case (DBDelimiters.HASH_CHAR):
-                r.addFieldValue(sb.toString());
-                return i;
-                
-            default:
-                sb.append(record.charAt(i));
+
+                case (DBDelimiters.PIPE_CHAR):
+                    r.addFieldValue(sb.toString());
+                    sb = new StringBuilder();
+
+                    break;
+
+                case (DBDelimiters.HASH_CHAR):
+                    r.addFieldValue(sb.toString());
+                    return i;
+
+                default:
+                    sb.append(record.charAt(i));
             }
         }
         r.addFieldValue(sb.toString());
         return record.length();
     }
-    
+
     /**
      * Update DataObject Child Types
      * @param parent - Parent Data Object
@@ -262,40 +256,40 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
      */
     private void updateDataObjectChildTypes(DataObject parent, String record) {
         StringBuilder sb = new StringBuilder();
-        
+
         ChildType ct = null;
         DataObject child = null;
         for (int i = 0; i < record.length(); i++) {
             switch (record.charAt(i)) {
-                
-            case (DBDelimiters.PIPE_CHAR):
-                child.addFieldValue(sb.toString());
-                sb = new StringBuilder();
-                
-                break;
-                
-            case (DBDelimiters.DOLLAR_CHAR):
-                if (child != null) {
+
+                case (DBDelimiters.PIPE_CHAR):
                     child.addFieldValue(sb.toString());
                     sb = new StringBuilder();
-                }
-                child = new DataObject();
-                ct.addChild(child);
-                break;
-            case (DBDelimiters.HASH_CHAR):
-                ct = new ChildType();
-                parent.addChildType(ct);
-                
-                break;
-                
-            default:
-                sb.append(record.charAt(i));
+
+                    break;
+
+                case (DBDelimiters.DOLLAR_CHAR):
+                    if (child != null) {
+                        child.addFieldValue(sb.toString());
+                        sb = new StringBuilder();
+                    }
+                    child = new DataObject();
+                    ct.addChild(child);
+                    break;
+                case (DBDelimiters.HASH_CHAR):
+                    ct = new ChildType();
+                    parent.addChildType(ct);
+
+                    break;
+
+                default:
+                    sb.append(record.charAt(i));
             }
         }
-        
+
         child.addFieldValue(sb.toString());
     }
-    
+
     /**
      * This method takes care of finalizing data objectnode created by Query Manager.
      * @param dataObjectNode
@@ -303,9 +297,9 @@ public abstract class BaseDBDataReader implements GlobalDataObjectReader {
     public void submitObjectForFinalization(Object candidateObject) {
         usedObjectsList.add(candidateObject);
     }
-    
-    protected void stopObjectFinalizer(){
+
+    protected void stopObjectFinalizer() {
+        sLog.infoNoloc("Stopping Finalizer with un-finalized objects. No of objects remaining : " + usedObjectsList.size());
         this.objfin.stopObjectFinalizer();
-    }    
-    
+    }
 }
