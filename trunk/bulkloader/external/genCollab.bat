436a2e3fc2b3cabbdfec5804adb0d25a3863be0e
bharanim <bharanim@77a3c157-be3e-0410-b79f-aee7da2dc004>
2008-04-22 07:52:53 +0000

    Changes to Bulk Loader generate scripts due to change in netbeans packaging structure.

--- trunk/bulkloader/external/genCollab.bat
+++ trunk/bulkloader/external/genCollab.bat
@@ -10,7 +10,7 @@ set DB_DRIVER_PATH=<Set DB Driver Path e.g. D:\Software-downloads\OJDBC1.4>
 set DB_DRIVER_NAME=<Set DB Driver Name e.g. ojdbc14-10.1.0.2.0.jar>
 
 	REM #### SOURCE DATABASE ####
-set SOURCE_LOC=<Specify Source Dir e.g. D:\temp\mural\masterindextest>
+set SOURCE_LOC=<Specify Source Dir e.g. D:\temp\mural\masterindex_image_files>
 set FIELD_DELIMITER="|"
 set RECORD_DELIMITER="$$$"
 
@@ -39,10 +39,10 @@ copy %DB_DRIVER_JAR% .\lib\%DB_DRIVER_NAME%
 set PATH=%JAVA_HOME%\bin;%PATH%
 set BLK=%CD%
 set USER_LIBS=%BLK%\lib\avalon-framework-4.1.3.jar;%BLK%\lib\axion-1.0.jar;%BLK%\lib\etl-editor-1.0.jar;%BLK%\lib\etl-engine-1.0.jar;%BLK%\lib\ETLEngineInvoker-1.0.jar;%BLK%\lib\i18n-1.0.jar;%BLK%\lib\ojdbc14-10.1.0.2.0.jar;%BLK%\lib\org-netbeans-modules-db-1.0.jar;%BLK%\lib\velocity-1.4.jar;%BLK%\lib\velocity-dep-1.4.jar;%BLK%\bulkloader-1.0.jar
-set OPENIDE_LIB_MODULE=%NB_HOME%\platform7\modules
-set OPENIDE_LIB_LIB=%NB_HOME%\platform7\lib
-set OPENIDE_LIB_CORE=%NB_HOME%\platform7\core
-set OPENIDE_LIB_IDE8_MOD=%NB_HOME%\ide8\modules
+set OPENIDE_LIB_MODULE=%NB_HOME%\platform8\modules
+set OPENIDE_LIB_LIB=%NB_HOME%\platform8\lib
+set OPENIDE_LIB_CORE=%NB_HOME%\platform8\core
+set OPENIDE_LIB_IDE8_MOD=%NB_HOME%\ide9\modules
 set OPENIDE_LIBS=%OPENIDE_LIB_MODULE%\org-openide-nodes.jar;%OPENIDE_LIB_MODULE%\org-openide-text.jar;%OPENIDE_LIB_MODULE%\org-openide-loaders.jar;%OPENIDE_LIB_MODULE%\org-openide-windows.jar;%OPENIDE_LIB_MODULE%\org-openide-dialogs.jar;%OPENIDE_LIB_MODULE%\org-openide-awt.jar;%OPENIDE_LIB_CORE%\org-openide-filesystems.jar;%OPENIDE_LIB_LIB%\org-openide-util.jar;%OPENIDE_LIB_IDE8_MOD%\org-netbeans-modules-db.jar
 set ALL_LIBS=%USER_LIBS%;%OPENIDE_LIBS%;%DB_DRIVER_JAR%;%CLASSPATH%
 java -Xms128m -Xmx512m -Dsourcedb.loc=%SOURCE_LOC% -Dfield.delimiter=%FIELD_DELIMITER% -Drecord.delimiter=%RECORD_DELIMITER% -Dtarget.type=%TARGET_DB_TYPE% -Dtarget.host=%TARGET_LOC% -Dtarget.port=%TARGET_PORT% -Dtarget.id=%TARGET_ID% -Dtarget.schema=%TARGET_SCHEMA% -Dtarget.catalog=%TARGET_CATALOG% -Dtarget.login=%TARGET_LOGIN% -Dtarget.pw=%TARGET_PW% -cp %ALL_LIBS% com.sun.dm.di.bulkloader.LoaderMain
--- trunk/bulkloader/external/genCollab.sh
+++ trunk/bulkloader/external/genCollab.sh
@@ -39,10 +39,10 @@ copy $DB_DRIVER_JAR ./lib/$DB_DRIVER_NAME
 PATH=$JAVA_HOME/bin;$PATH
 BLK=$CD
 USER_LIBS=$BLK/lib/avalon-framework-4.1.3.jar;$BLK/lib/axion-1.0.jar;$BLK/lib/etl-editor-1.0.jar;$BLK/lib/etl-engine-1.0.jar;$BLK/lib/ETLEngineInvoker-1.0.jar;$BLK/lib/i18n-1.0.jar;$BLK/lib/ojdbc14-10.1.0.2.0.jar;$BLK/lib/org-netbeans-modules-db-1.0.jar;$BLK/lib/velocity-1.4.jar;$BLK/lib/velocity-dep-1.4.jar;$BLK/bulkloader-1.0.jar
-OPENIDE_LIB_MODULE=$NB_HOME/platform7/modules
-OPENIDE_LIB_LIB=$NB_HOME/platform7/lib
-OPENIDE_LIB_CORE=$NB_HOME/platform7/core
-OPENIDE_LIB_IDE8_MOD=$NB_HOME/ide8/modules
+OPENIDE_LIB_MODULE=$NB_HOME/platform8/modules
+OPENIDE_LIB_LIB=$NB_HOME/platform8/lib
+OPENIDE_LIB_CORE=$NB_HOME/platform8/core
+OPENIDE_LIB_IDE8_MOD=$NB_HOME/ide9/modules
 OPENIDE_LIBS=$OPENIDE_LIB_MODULE/org-openide-nodes.jar;$OPENIDE_LIB_MODULE/org-openide-text.jar;$OPENIDE_LIB_MODULE/org-openide-loaders.jar;$OPENIDE_LIB_MODULE/org-openide-windows.jar;$OPENIDE_LIB_MODULE/org-openide-dialogs.jar;$OPENIDE_LIB_MODULE/org-openide-awt.jar;$OPENIDE_LIB_CORE/org-openide-filesystems.jar;$OPENIDE_LIB_LIB/org-openide-util.jar;$OPENIDE_LIB_IDE8_MOD/org-netbeans-modules-db.jar
 ALL_LIBS=$USER_LIBS;$OPENIDE_LIBS;$DB_DRIVER_JAR;$CLASSPATH
 java -Xms128m -Xmx512m -Dsourcedb.loc=$SOURCE_LOC -Dfield.delimiter=$FIELD_DELIMITER -Drecord.delimiter=$RECORD_DELIMITER -Dtarget.type=$TARGET_DB_TYPE -Dtarget.host=$TARGET_LOC -Dtarget.port=$TARGET_PORT -Dtarget.id=$TARGET_ID -Dtarget.schema=$TARGET_SCHEMA -Dtarget.catalog=$TARGET_CATALOG -Dtarget.login=$TARGET_LOGIN -Dtarget.pw=$TARGET_PW -cp $ALL_LIBS com.sun.dm.di.bulkloader.LoaderMain
