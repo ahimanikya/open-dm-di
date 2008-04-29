249e6afd2d6c8350e4d1d571b53d61dd67b7382c
bharanim <bharanim@77a3c157-be3e-0410-b79f-aee7da2dc004>
2008-04-29 12:33:48 +0000

    Updating License Headers

--- trunk/plugin/src/main/java/com/sun/dm/dimi/plugin/Main.java
+++ trunk/plugin/src/main/java/com/sun/dm/dimi/plugin/Main.java
@@ -1,4 +1,27 @@
 /*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2007 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * The contents of this file are subject to the terms of the Common 
+ * Development and Distribution License ("CDDL")(the "License"). You 
+ * may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * https://open-dm-mi.dev.java.net/cddl.html
+ * or open-dm-mi/bootstrap/legal/license.txt. See the License for the 
+ * specific language governing permissions and limitations under the  
+ * License.  
+ *
+ * When distributing the Covered Code, include this CDDL Header Notice 
+ * in each file and include the License file at
+ * open-dm-mi/bootstrap/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the 
+ * fields enclosed by brackets [] replaced by your own identifying 
+ * information: "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
+/*
  * Main.java
  *
  * Created on Jul 17, 2007, 2:31:59 PM
@@ -53,12 +76,12 @@ public class Main {
     
     public static void main(String[] args) throws InterruptedException, ParserException, FileNotFoundException{
         String howmany = "10K";
-        boolean configset = DataSourceReaderFactory.setEViewConfigFilePath("D:/kishor_s1", "object.xml");
+        boolean configset = DataSourceReaderFactory.setEViewConfigFilePath("D:/temp/AAA3/mycompany/files-generated/resource", "object.xml");
 
         if (configset){
             // DB READER AND WRITER
-            GlobalDataObjectReader doReader = DataSourceReaderFactory.getNewDataObjectReader("PERSONPHONETEST", "D:/kishor_s1/PersonPh0124", null, true);
-            DOWriter doWriter = DataObjectWriterFactory.getNewDataObjectWriter("D:/kishor_new", "File1_" + howmany  + ".txt", true);
+            GlobalDataObjectReader doReader = DataSourceReaderFactory.getNewDataObjectReader("COMPANY", "D:/temp/AAA3/db", null, true);
+            DOWriter doWriter = DataObjectWriterFactory.getNewDataObjectWriter("D:/temp/Abhijeet/files", "File1_" + howmany  + ".txt", true);
             
             //FILE READER AND WRITER
             //GlobalDataObjectReader doReader = DataSourceReaderFactory.getNewDataObjectReader("C:/Documents and Settings/Manish/Desktop/terry", "good.txt", true, true);
@@ -73,8 +96,8 @@ public class Main {
                     if (dobj == null) break;
                     ++count;
                     if (count%1 == 0){
-                        //System.out.println("Fetching Data Object  << << << " + count);
-                        //System.out.println("MANISH DO :: \n" + dobj.toString());
+                        System.out.println("Fetching Data Object  << << << " + count);
+                        System.out.println("MANISH DO :: \n" + dobj.toString());
                     }
                     doWriter.write(dobj);
                     doReader.submitObjectForFinalization(dobj);
