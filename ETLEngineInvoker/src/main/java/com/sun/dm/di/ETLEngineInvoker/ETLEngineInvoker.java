ead263e19b0f618cda8c347a62b2feef7f9d5bc4
bharanim <ahimanikya@users.noreply.github.com>
2008-05-19 08:20:47 +0000

    Refactoring component and cleaning up unwanted portions.

--- ETLEngineInvoker/src/main/java/com/sun/dm/di/ETLEngineInvoker/ETLEngineInvoker.java
+++ ETLEngineInvoker/src/main/java/com/sun/dm/di/ETLEngineInvoker/ETLEngineInvoker.java
@@ -22,17 +22,22 @@
  */
 package com.sun.dm.di.ETLEngineInvoker;
 
+import com.sun.dm.di.ETLEngineInvoker.bulkloader.TargetDBOperations;
 import com.sun.etl.engine.ETLEngine;
 import com.sun.etl.engine.ETLEngineContext;
 import com.sun.etl.engine.ETLEngineExecEvent;
 import com.sun.etl.engine.ETLEngineListener;
 import com.sun.etl.engine.ETLEngineLogEvent;
 import com.sun.etl.engine.impl.ETLEngineImpl;
+import com.sun.sql.framework.utils.ScEncrypt;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -53,7 +58,14 @@ public class ETLEngineInvoker {
      * Logger instance
      */
     private static transient final Logger mLogger = Logger.getLogger("ETLEngineInvoker");
+    private static String TRGT_SCHEMA = null;
+    private static String TRGT_DB_CONN = null;
+    private static String TRGT_DB_LOGIN = null;
+    private static String TRGT_DB_PW = null;
     private static String ENGINE_FILE = "C:\\ETL\\ETLEngineInvoker\\engine.xml";
+    private static TargetDBOperations trgtdbopr = null;
+    private static Connection trgtconn = null;
+    private static int dbtype = 0;
     private ETLEngine engine = null;
     private ETLEngineListener listener = null;
     static LogManager logManager = null;
@@ -62,44 +74,74 @@ public class ETLEngineInvoker {
     }
 
     /*private void startProcessing(File engineFile) {
-        ETLWorkerThread worker = new ETLWorkerThread(engineFile);
-        Thread workerThread = new Thread(worker);
-        workerThread.start();
+    ETLWorkerThread worker = new ETLWorkerThread(engineFile);
+    Thread workerThread = new Thread(worker);
+    workerThread.start();
     }*/
-    
     private void startProcessing(File engineFile) {
-        
+
         executeEngine(engineFile);
     }
 
     public static void main(String[] args) {
         confiugreLogger();
-        String engineFileName ="";
+        String engineFileName = "";
         try {
-            if (args.length >= 1) {
-                ENGINE_FILE = args[0];
+            if (args.length >= 5) {
+                TRGT_SCHEMA = args[0];
+                TRGT_DB_CONN = args[1];
+                TRGT_DB_LOGIN = args[2];
+                TRGT_DB_PW = args[3];
+                ENGINE_FILE = args[4];
             }
-            ArrayList<String> arrFiles = new ArrayList<String>();
-            BufferedReader input = new BufferedReader(new FileReader(ENGINE_FILE));
-            String fileName = "";
-            while (fileName != null) {
-                fileName = input.readLine();
-                if (fileName != null && fileName.trim().length() > 0) {
-                    arrFiles.add(fileName);
+
+            trgtconn = connectToDB(TRGT_DB_CONN, TRGT_DB_LOGIN, TRGT_DB_PW);
+
+            if (trgtconn != null) {
+                trgtdbopr = new TargetDBOperations();
+
+                ArrayList<String> arrFiles = new ArrayList<String>();
+                BufferedReader input = new BufferedReader(new FileReader(ENGINE_FILE));
+                String fileName = "";
+                while (fileName != null) {
+                    fileName = input.readLine();
+                    if (fileName != null && fileName.trim().length() > 0) {
+                        arrFiles.add(fileName);
+                    }
                 }
-            }
-            input.close();
-            for (int i = 0; i < arrFiles.size(); i++) {
-                engineFileName = arrFiles.get(i);
-                mLogger.info("Start executing Engine File :: " + engineFileName );
-                ETLEngineInvoker invoker = new ETLEngineInvoker();
-                
-                invoker.startProcessing(new File(arrFiles.get(i)));
-                 mLogger.info("End executing Engine File :: " + engineFileName );
+                input.close();
+
+                // Disable Constraints for All the Tables Being processed
+                for (int i = 0; i < arrFiles.size(); i++) {
+                    trgtdbopr.disableConstrains(TRGT_SCHEMA, trgtconn, arrFiles.get(i), dbtype);
+                }
+
+                // Load Data
+                for (int i = 0; i < arrFiles.size(); i++) {
+                    engineFileName = arrFiles.get(i);
+                    mLogger.info("Start executing ETL Engine File :: " + engineFileName);
+                    ETLEngineInvoker invoker = new ETLEngineInvoker();
+
+                    invoker.startProcessing(new File(".\\ETLProcess\\" + arrFiles.get(i) + "\\DefaultETL_engine.xml"));
+                    mLogger.info("End executing ETL Engine File :: " + engineFileName);
+                }
+
+                // Enable Constraints for All the Tables Being processed
+                for (int i = 0; i < arrFiles.size(); i++) {
+                    trgtdbopr.enableConstrains(TRGT_SCHEMA, trgtconn, arrFiles.get(i), dbtype);
+                }
+            } else {
+                mLogger.severe("Unable to Connect to Target Database :: " + TRGT_DB_CONN);
             }
 
         } catch (Exception ex) {
-            Logger.getLogger("global").log(Level.SEVERE, null, ex);
+            mLogger.severe(ex.getMessage());
+        } finally {
+            try {
+                trgtconn.close();
+            } catch (SQLException ex) {
+                mLogger.warning("Unable to close target connection : " + ex.getMessage());
+            }
         }
     }
 
@@ -131,48 +173,89 @@ public class ETLEngineInvoker {
             Logger.getLogger("global").log(Level.SEVERE, null, ex);
         }
     }
-/*
+    /*
     private class ETLWorkerThread implements Runnable {
-
-        private File engineFile = null;
-
-        public ETLWorkerThread(File file) {
-            engineFile = file;
-            init();
-        }
-
-        private void init() {
-            try {
-                System.out.println("Setting Engine context...");
-                engine = new ETLEngineImpl();
-                ETLEngineContext context = new ETLEngineContext();
-                System.out.println("Parsing Engine File...");
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder docBuilder = factory.newDocumentBuilder();
-                Document doc = docBuilder.parse(new FileInputStream(engineFile));
-                engine.setContext(context);
-                engine.setRunningOnAppServer(true);
-                engine.parseXML(doc.getDocumentElement());
-                System.out.println("Initializing engine listener...");
-                listener = new ETLEngineListenerImpl();
-            } catch (Exception ex) {
-                Logger.getLogger("global").log(Level.SEVERE, null, ex);
-            }
-        }
-
-        public void run() {
-            System.out.println("Transferring control to ETL Engine ...");
-            engine.exec(listener);
-            synchronized (listener) {
-                try {
-                    listener.wait();
-                } catch (Exception ex) {
-                    Logger.getLogger("global").log(Level.SEVERE, null, ex);
-                }
-            }
-        }
+    
+    private File engineFile = null;
+    
+    public ETLWorkerThread(File file) {
+    engineFile = file;
+    init();
     }
-*/
+    
+    private void init() {
+    try {
+    System.out.println("Setting Engine context...");
+    engine = new ETLEngineImpl();
+    ETLEngineContext context = new ETLEngineContext();
+    System.out.println("Parsing Engine File...");
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder docBuilder = factory.newDocumentBuilder();
+    Document doc = docBuilder.parse(new FileInputStream(engineFile));
+    engine.setContext(context);
+    engine.setRunningOnAppServer(true);
+    engine.parseXML(doc.getDocumentElement());
+    System.out.println("Initializing engine listener...");
+    listener = new ETLEngineListenerImpl();
+    } catch (Exception ex) {
+    Logger.getLogger("global").log(Level.SEVERE, null, ex);
+    }
+    }
+    
+    public void run() {
+    System.out.println("Transferring control to ETL Engine ...");
+    engine.exec(listener);
+    synchronized (listener) {
+    try {
+    listener.wait();
+    } catch (Exception ex) {
+    Logger.getLogger("global").log(Level.SEVERE, null, ex);
+    }
+    }
+    }
+    }
+     */
+
+    private static Connection connectToDB(String dbconnstr, String login, String pw) {
+        pw = ScEncrypt.decrypt("soabi", pw);
+        if (dbconnstr.indexOf("axion") != -1) {
+            System.out.println("Init Axion Driver");
+            try {
+                Class.forName("org.axiondb.jdbc.AxionDriver");
+                return DriverManager.getConnection(dbconnstr, login, pw);
+            } catch (SQLException ex) {
+                System.out.println("Axion driver SQL exception :: " + ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Axion driver Class not found exception :: " + ex.getMessage());
+            }
+        } else if (dbconnstr.indexOf("oracle") != -1) {
+            dbtype = 1;
+            System.out.println("Init Oracle Driver");
+            try {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                return DriverManager.getConnection(dbconnstr, login, pw);
+            } catch (SQLException ex) {
+                System.out.println("Oracle SQL exception :: " + ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Oracle driver Class not found exception :: " + ex.getMessage());
+            }
+        } else if (dbconnstr.indexOf("sqlserver") != -1) {
+            dbtype = 3;
+            System.out.println("Init SQL Server Driver");
+            try {
+                Class.forName("com.SeeBeyond.sqlserver.jdbc.sqlserver.SQLServerDriver");
+                return DriverManager.getConnection(dbconnstr, login, pw);
+            } catch (SQLException ex) {
+                System.out.println("SQL Server SQL exception :: " + ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                System.out.println("SQL Server driver Class not found exception :: " + ex.getMessage());
+            }
+        }
+
+        return null;
+
+    }
+
     private class ETLEngineListenerImpl implements ETLEngineListener {
 
         public void executionPerformed(ETLEngineExecEvent event) {
@@ -191,24 +274,24 @@ public class ETLEngineInvoker {
                     String tblName = it.next();
                     System.out.println("");
                     System.out.println("\t" + i++ + "." + tblName);
-                    System.out.println("\t\tRows Extracted: " + engine.getContext().getStatistics().getRowsExtractedCount(tblName));
+                    //System.out.println("\t\tRows Extracted: " + engine.getContext().getStatistics().getRowsExtractedCount(tblName));
                     System.out.println("\t\tRows Inserted: " + engine.getContext().getStatistics().getRowsInsertedCount(tblName));
-                    System.out.println("\t\tRows Rejected: " + engine.getContext().getStatistics().getRowsRejectedCount(tblName));
+                    //System.out.println("\t\tRows Rejected: " + engine.getContext().getStatistics().getRowsRejectedCount(tblName));
                     rowsExtracted += engine.getContext().getStatistics().getRowsExtractedCount(tblName);
                     rowsRejected += engine.getContext().getStatistics().getRowsRejectedCount(tblName);
                     rowsInserted += engine.getContext().getStatistics().getRowsInsertedCount(tblName);
                 }
                 System.out.println("");
-                System.out.println("Total Rows Extracted for " + (i - 1) + " table(s) :" + rowsExtracted);
+                //System.out.println("Total Rows Extracted for " + (i - 1) + " table(s) :" + rowsExtracted);
                 System.out.println("Total Rows Inserted for " + (i - 1) + " table(s) :" + rowsInserted);
-                System.out.println("Total Rows Rejected for " + (i - 1) + " table(s) :" + rowsRejected);
+                //System.out.println("Total Rows Rejected for " + (i - 1) + " table(s) :" + rowsRejected);
                 System.out.println("");
                 System.out.println("Start time:" + startTime.toString());
                 System.out.println("End time:" + endTime.toString());
                 System.out.println("");
                 long diff = endTime.getTime() - startTime.getTime();
                 float timeTaken = diff / 1000;
-                System.out.println("Total time taken:" + String.valueOf(diff) + " milli seconds");
+                //System.out.println("Total time taken:" + String.valueOf(diff) + " milli seconds");
                 System.out.println("Total time taken:" + String.valueOf(timeTaken) + " seconds");
                 System.out.println("");
                 System.out.println("Exiting ETL Engine...");
@@ -223,7 +306,7 @@ public class ETLEngineInvoker {
     }
 
     public static void confiugreLogger() {
-        System.out.println("Configuring the logger ..................");
+        System.out.println("Configuring the logger ...");
         FileInputStream ins = null;
         try {
             //LogManager logManager;
@@ -231,9 +314,9 @@ public class ETLEngineInvoker {
             File f = new File("./logs");
 
             if (f.exists() && f.isDirectory()) {
-                mLogger.info("logger dir exist");
+                //mLogger.info("logger dir exist");
             } else {
-                mLogger.info("creating new logger dir");
+                //mLogger.info("creating new logger dir");
                 f.mkdir();
             }
             if (logManager == null) {
