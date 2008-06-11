000e8e3d158fb10fcad166542010b96d6fda9e0d
nilesh_apte <nilesh_apte@77a3c157-be3e-0410-b79f-aee7da2dc004>
2008-06-11 18:58:01 +0000

    Removed runtime and reverted changes to ETLEngineInvoker and bulkloader

--- trunk/runtime/packaging/src/scripts/derby/create_etlse_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE MESSAGEPIPELINE (
-    exchangeid varchar(128),
-    serviceName varchar(128),
-    operationName varchar(128),
-    content varchar(8192),
-    status INTEGER, 
-    primary key (exchangeid)
-);
